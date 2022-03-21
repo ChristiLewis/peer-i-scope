e8b810fd9985f85d705223ab1ad3bff7debde193
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-03-20 21:33:19 -0700

    db-tables seeded

--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,27 @@
-/* DATABASE TABLE */
+/* DEPARTMENT TABLE */
+
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     dept_name VARCHAR(30) NOT NULL
 );
 
 /* JOB ROLE TABLE */
+
 CREATE TABLE job_role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(000000.00) NOT NULL,
-    dept_id INTEGER
+    salary DECIMAL(8,2) NOT NULL,
+    dept_id INTEGER,
+    FOREIGN KEY (dept_id) REFERENCES department(id)
 );
 
 /* EMPLOYEE TABLE */
+
 CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     job_role_id INTEGER,
-    manager_id INTEGER NULL
+    FOREIGN KEY (job_role_id) REFERENCES job_role(id), 
+    manager_id VARCHAR(30)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -7,6 +7,14 @@ VALUES
 
 INSERT INTO job_role (title, salary, dept_id)
 VALUES
-    ('Field-technician', 52,000.00, 1),
-    ('Supervisor', 41,600.00, 1),
-    ('Architect', 87,360.00, 4);
+    ('Field Technician', 52000.00, 1),
+    ('Supervisor', 41600.00, 2),
+    ('Project Manager', 62400.00, 3),
+    ('Architect', 87360.00, 4);
+
+INSERT INTO employee (first_name, last_name, job_role_id, manager_id)
+VALUES
+    ('John', 'Handyman', 1, 'Sally Presentswell'),
+    ('Paul', 'Likable', 2,'Sally Presentswell'),
+    ('Sally', 'Presentswell', 3, NULL),
+    ('George', 'Problemsolver', 4, NULL);
